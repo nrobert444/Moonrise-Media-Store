f38e131d978aeebf32e980c7a2b7a2e1d5950083
Nicholas Roberto <njr444@gmail.com>
2020-10-01 11:31:31 -0700

    created loader component and message component to display error message.

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import Product from '../components/Product'
 import { Row, Col } from 'react-bootstrap'
 import { listProducts } from '../actions/productActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 const HomePage = () => {
   const dispatch = useDispatch()
@@ -16,9 +18,9 @@ const HomePage = () => {
     <>
       <h1>Latest Products</h1>
       {loading ? (
-        <h2>'Loading ...'</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant='danger'>{error}</Message>
       ) : (
         <Row>
           {products.map(product => (
