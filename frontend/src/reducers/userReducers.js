dcbf930f5f99ca3b39b302dd03b940c26c1d05fc
Nicholas Roberto <njr444@gmail.com>
2020-10-02 11:28:27 -0700

    created actions for user profile.

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -38,12 +38,12 @@ export const userRegisterReducer = (state = {}, action) => {
       return state
   }
 }
-export const userDetailsReducer = (state = {}, action) => {
+export const userDetailsReducer = (state = { user: {} }, action) => {
   switch (action.type) {
     case USER_DETAILS_REQUEST:
-      return { loading: true }
+      return { ...state, loading: true }
     case USER_DETAILS_SUCCESS:
-      return { loading: false, userInfo: action.payload }
+      return { loading: false, user: action.payload }
     case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload }
     default:
