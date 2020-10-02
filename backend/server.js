1167cda6bc3203a7076295838c80689fbfc291ab
Nicholas Roberto <njr444@gmail.com>
2020-10-02 08:57:55 -0700

    added routes for creating a new user and hashing/salting password. Added auth middleware for protecting jwt token.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import connectDb from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import dotenv from 'dotenv'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
@@ -10,11 +11,14 @@ dotenv.config()
 const app = express()
 connectDb()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('API is running')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(errorHandler)
 app.use(notFound)
