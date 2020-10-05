b99de9c113cc8a131f1fb4a28eb6a8d444bdc62b
Nicholas Roberto <njr444@gmail.com>
2020-10-05 11:41:26 -0700

    created addorderitems controller and order items routes then added to server.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import connectDb from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 import dotenv from 'dotenv'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
@@ -19,6 +20,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 app.use(errorHandler)
 app.use(notFound)
