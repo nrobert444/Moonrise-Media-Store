dbaf72526a2586536b7415b2529256e5b1701a4a
Nicholas Roberto <njr444@gmail.com>
2020-10-05 09:33:05 -0700

    created action for saving user shipping address, added to reducer and implemented to the shipping page. Information was saved to localstorage for data persistence.

--- frontend/src/Pages/ShippingPage.js
+++ frontend/src/Pages/ShippingPage.js
@@ -2,16 +2,22 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Button, Form } from 'react-bootstrap'
 import FormContainer from '../components/FormContainer'
+import { saveShippingAddress } from '../actions/cartActions'
 
 const ShippingPage = ({ history }) => {
-  const [address, setAddress] = useState('')
-  const [city, setCity] = useState('')
-  const [postalCode, setPostalCode] = useState('')
-  const [country, setCountry] = useState('')
+  const cart = useSelector(state => state.cart)
+  const { shippingAddress } = cart
+  const [address, setAddress] = useState(shippingAddress.address)
+  const [city, setCity] = useState(shippingAddress.city)
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode)
+  const [country, setCountry] = useState(shippingAddress.country)
+
+  const dispatch = useDispatch()
 
   const submitHandler = e => {
     e.preventDefault()
-    console.log('shipping submitted')
+    dispatch(saveShippingAddress({ address, city, postalCode, country }))
+    history.push('/payment')
   }
   return (
     <FormContainer>
