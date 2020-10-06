68d9c9e4a54ad2ef6d9d84ac54baa1080c23f742
Nicholas Roberto <njr444@gmail.com>
2020-10-06 14:02:01 -0700

    added order reset and user details reset constants/reducers so no data persists across users login / logout.

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -11,7 +11,8 @@ import {
   ORDER_PAY_REQUEST,
   ORDER_LIST_MY_REQUEST,
   ORDER_LIST_MY_SUCCESS,
-  ORDER_LIST_MY_FAIL
+  ORDER_LIST_MY_FAIL,
+  ORDER_LIST_MY_RESET
 } from '../constants/orderConstants'
 
 export const orderCreateReducers = (state = {}, action) => {
@@ -73,7 +74,8 @@ export const orderListMyReducer = (state = { orders: [] }, action) => {
       }
     case ORDER_LIST_MY_FAIL:
       return { loading: false, error: action.payload }
-
+    case ORDER_LIST_MY_RESET:
+      return { orders: [] }
     default:
       return state
   }
