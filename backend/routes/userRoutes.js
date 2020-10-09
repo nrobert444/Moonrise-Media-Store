3450c01b0f784276e4a0c7240dabaf530046114b
Nicholas Roberto <njr444@gmail.com>
2020-10-09 10:34:43 -0700

    added backend controller and route for admin to delete a user from the database.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,17 +4,18 @@ import {
   getUserProfile,
   registerUser,
   updateUserProfile,
-  getUsers
+  getUsers,
+  deleteUser
 } from '../controllers/userController.js'
 import { protect, isAdmin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/').post(registerUser).get(protect, isAdmin, getUsers)
-router.post('/login', authUser)
 router
   .route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile)
+router.route('/:id').delete(protect, isAdmin, deleteUser)
 
 export default router
