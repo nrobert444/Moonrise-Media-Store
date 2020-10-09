550854ea2fc59f45ee119610fb54d1ab1ff49327
Nicholas Roberto <njr444@gmail.com>
2020-10-09 10:05:59 -0700

    updated user list page useEffect to conditionally render the user list if login is an admin. added reset action creater to reducer on logout.

--- frontend/src/Pages/UserListPage.js
+++ frontend/src/Pages/UserListPage.js
@@ -5,14 +5,20 @@ import { Table, Button } from 'react-bootstrap'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 import { listUsers } from '../actions/userActions'
-const UserListPage = () => {
+const UserListPage = ({ history }) => {
   const dispatch = useDispatch()
   const userList = useSelector(state => state.userList)
   const { loading, error, users } = userList
+  const userLogin = useSelector(state => state.userLogin)
+  const { userInfo } = userLogin
 
   useEffect(() => {
-    dispatch(listUsers())
-  }, [dispatch])
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers())
+    } else {
+      history.push('/login')
+    }
+  }, [dispatch, history])
 
   const deleteHandler = id => {
     console.log('user deleted')
