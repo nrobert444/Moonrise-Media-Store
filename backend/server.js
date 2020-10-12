9442f98e206556cda3585e4788e75b1427cdf179
Nicholas Roberto <njr444@gmail.com>
2020-10-12 12:58:33 -0700

    added upload folder and installed multer for file image upload. Completed multer config and route, then added to server with static folder for express to access.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import path from 'path'
 import connectDb from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 import dotenv from 'dotenv'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
@@ -21,10 +23,14 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+const __dirname = path.resolve()
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(errorHandler)
 app.use(notFound)
