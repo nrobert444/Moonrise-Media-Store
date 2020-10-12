7ea149cc70f9b528c7b4b00dbb99da6290773c24
Nicholas Roberto <njr444@gmail.com>
2020-10-12 10:02:54 -0700

    added constants, reducer and action for frontend admin product delete.

--- frontend/src/Pages/ProductListPage.js
+++ frontend/src/Pages/ProductListPage.js
@@ -4,13 +4,20 @@ import { LinkContainer } from 'react-router-bootstrap'
 import { Table, Button, Row, Col } from 'react-bootstrap'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
-import { listProducts } from '../actions/productActions'
+import { listProducts, deleteProduct } from '../actions/productActions'
 
 const ProductListPage = ({ history, match }) => {
   const dispatch = useDispatch()
 
   const productList = useSelector(state => state.productList)
   const { loading, error, products } = productList
+
+  const productDelete = useSelector(state => state.productDelete)
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete
+  } = productDelete
 
   const userLogin = useSelector(state => state.userLogin)
   const { userInfo } = userLogin
@@ -21,11 +28,11 @@ const ProductListPage = ({ history, match }) => {
     } else {
       history.push('/login')
     }
-  }, [dispatch, history, userInfo])
+  }, [dispatch, history, userInfo, successDelete])
 
   const deleteHandler = id => {
     if (window.confirm('Are you sure?')) {
-      // dispatch(deleteProduct(id))
+      dispatch(deleteProduct(id))
     }
   }
   const createProductHandler = product => {
@@ -44,6 +51,8 @@ const ProductListPage = ({ history, match }) => {
           </Button>
         </Col>
       </Row>
+      {loadingDelete && <Loader />}
+      {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
       {loading ? (
         <Loader />
       ) : error ? (
