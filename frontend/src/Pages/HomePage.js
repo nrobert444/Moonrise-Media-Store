861f243a9110267fdc5875f3385a0a5cb8f5d19b
Nicholas Roberto <njr444@gmail.com>
2020-10-13 14:12:25 -0700

    added paginate component to homepage to view items by page and also by search keyword.

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -5,13 +5,14 @@ import { Row, Col } from 'react-bootstrap'
 import { listProducts } from '../actions/productActions'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
+import Paginate from '../components/Paginate'
 
 const HomePage = ({ match }) => {
   const keyword = match.params.keyword
   const pageNumber = match.params.pageNumber || 1
   const dispatch = useDispatch()
   const productList = useSelector(state => state.productList)
-  const { loading, error, products } = productList
+  const { loading, error, products, page, pages } = productList
   useEffect(() => {
     dispatch(listProducts(keyword, pageNumber))
   }, [dispatch, keyword, pageNumber])
@@ -24,13 +25,20 @@ const HomePage = ({ match }) => {
       ) : error ? (
         <Message variant='danger'>{error}</Message>
       ) : (
-        <Row>
-          {products.map(product => (
-            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
+        <>
+          <Row>
+            {products.map(product => (
+              <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate
+            page={page}
+            pages={pages}
+            keyword={keyword ? keyword : ''}
+          />
+        </>
       )}
     </>
   )
