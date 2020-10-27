5ba891799118405adf94d35e06260eb0002a6555
Nicholas Roberto <njr444@gmail.com>
2020-10-27 15:32:41 -0700

    updated footer to include links for about and contact and styled to resemble header.

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,16 +1,34 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Nav, Navbar } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Footer = () => {
   return (
     <footer>
-      <Container>
-        <Row>
-          <Col className='text-center py-3'>
-            Copyright &copy; Moonrise Media LLC
-          </Col>
-        </Row>
-      </Container>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse>
+            <Row>
+              <Col className='text-left py-3'>
+                Copyright &copy; Moonrise Media LLC
+              </Col>
+            </Row>
+            <Nav className='ml-auto'>
+              <LinkContainer to='/contact'>
+                <Nav.Link>
+                  <i className='fas fa-phone-square' /> Contact
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/about'>
+                <Nav.Link>
+                  <i className='fas fa-user' /> About
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </footer>
   )
 }
