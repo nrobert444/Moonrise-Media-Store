1ba63b0660272a554ab42cc51b25392cb0d1a32c
Nicholas Roberto <njr444@gmail.com>
2020-11-09 10:34:48 -0800

    added global styles for font and text-decoration.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { GlobalStyle } from './globalStyles'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import AboutPage from './Pages/AboutPage'
@@ -27,6 +28,7 @@ import ContactPage from './Pages/ContactPage'
 const App = () => {
   return (
     <Router>
+      <GlobalStyle />
       <Header />
       <main className='py-3'>
         <Container>
--- frontend/src/globalStyles.js
+++ frontend/src/globalStyles.js
@@ -5,7 +5,7 @@ body {
   font-family: Open Sans Condensed;
   padding: 20px 60px;
 
-  @media screen and (max-width: 800px) {
+  @media screen and (max-width: 1800px) {
       padding: 10px;
   }
 }
